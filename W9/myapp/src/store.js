3b741c6e61841133fc14feb66fc2c91b29d7fc99
Bartosz Chaber <bartosz.chaber@pw.edu.pl>
2023-06-21 18:34:57 +0200

    I dont remember why I did it

--- W9/myapp/src/store.js
+++ W9/myapp/src/store.js
@@ -1,4 +1,4 @@
-import { writable, derived } from "svelte/store";
+import { writable } from "svelte/store";
 
 export const authenticated = writable(false);
 export const authorized = writable(false);
@@ -8,13 +8,3 @@ export const token = writable();
 export const error = writable();
 
 export const tasks = writable([]);
-
-export const user_tasks = derived([tasks, user], ([$tasks, $user]) => {
-  let logged_in_user_tasks = [];
-
-  if ($user && $user.email) {
-    logged_in_user_tasks = $tasks.filter((task) => task.user === $user.email);
-  }
-
-  return logged_in_user_tasks;
-});
